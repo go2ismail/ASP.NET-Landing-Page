55a49d1f17679f73279f82426a29952b2ebe616a
go2ismail <go2ismail@gmail.com>
2024-03-19 13:19:23 +0700

    update readme

--- README.md
+++ README.md
@@ -5,5 +5,10 @@ Explore ASP.NET Landing Page, an open-source project offering a stunning and cus
 
 Live Demo Link: [https://landingpage.csharpasp.net/](https://landingpage.csharpasp.net/)
 
+For a fully functional experience, including complete authentication, contact services, and booking features, visit our store now [https://store.indotalent.com/](https://store.indotalent.com/)
+
+# License
+ASP.NET Landing Page is based on Startup HTML template from HTMLCODEX [LICENSE](LICENSE).
+
 
 
--- obj/Debug/net8.0/Indotalent.AssemblyInfo.cs
+++ obj/Debug/net8.0/Indotalent.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Indotalent")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4233c7772488900d0c50565397cbc68deefdbb77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c3b0a5a31b0c85ebdf1dab5efbaa2e166cab28a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Indotalent")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Indotalent")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
